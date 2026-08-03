60144a8b753c981ab0a0cf06753fb9aa03a99f96
Gabriella (Gabby) Hylton <gabriella.hylton@gmail.com>
2026-08-03 13:26:33 -0400

    font
    fixed fonts on opening screen

--- trail-blazing/src/components/SplashScreen.jsx
+++ trail-blazing/src/components/SplashScreen.jsx
@@ -3,8 +3,8 @@ import "./Signboard.css";
 export default function SplashScreen({ onStart }) {
     return (
         <div className="screen splash-screen">
-            <h1 className="trail-title">TRAIL BLAZER</h1>
-            <p className="trail-tagline">find a new path</p>
+            <h1 className="festive-regular">Trail   Blazer</h1>
+            <p className="ruge-boogie-regular">find a new path</p>
             <button className="btn-start" onClick={onStart}>
                 start here!
             </button>
